879c250dbc6c8e548ef977e25a45959a23d0e67e
Bakhytzhan Abeldinov <bakhytzhan79@mail.ru>
2022-12-07 21:14:20 +0600

    add change

--- file11.js
+++ file11.js
@@ -1,64 +1,62 @@
 class Animal {
-    constructor(name) {
-      this.name = name;
-    }
-    speak() {
-      console.log(`${this.name} says ${this.voice}.`);
-    }
-  
-    move(step) {
-      console.log(`${this.name} takes ${step} steps.`);
-    }
-  
-    belongsTo() {
-      console.log(`${this.name} is belong to ${this.owner}.`);
-    }
-  
+  constructor(name, owner, voice) {
+    this.name = name;
+    this.owner = owner;
+    this.voice = voice;
   }
-  
-  class Cat extends Animal{
-    constructor(name, owner) {
-      super(name);
-      this.owner = owner;
-      this.voice = "Meow";
-    }
-    
-  }
-  
-  class Dog extends Animal {
-    constructor(name, owner) {
-      super(name);
-      this.owner = owner;
-      this.voice = "Gaff";
-    }
+  speak() {
+    console.log(`${this.name} says ${this.voice}.`);
   }
 
-const whisker = new Cat("Whisker", "Bob");
+  move(step) {
+    console.log(`${this.name} takes ${step} steps.`);
+  }
+
+  belongsTo() {
+    console.log(`${this.name} is belong to ${this.owner}.`);
+  }
+
+}
+
+class Cat extends Animal{
+  constructor(name, owner, voice) {
+    super(name, owner, voice);
+  }
+  
+}
+
+class Dog extends Animal {
+  constructor(name, owner, voice) {
+    super(name, owner, voice);
+  }
+}
+
+const whisker = new Cat("Whisker", "Bob", "Hello!");
 whisker.speak();
 whisker.move(5);
 whisker.belongsTo();
 
-const felix = new Cat("Felix", "Tom");
+const felix = new Cat("Felix", "Tom", "Hello!");
 felix.speak();
 felix.move(5);
 felix.belongsTo();
 
-const leopold = new Cat("Leopold", "Jack");
+const leopold = new Cat("Leopold", "Jack", "Hello!");
 leopold.speak();
 leopold.move(5);
 leopold.belongsTo();
 
-const beethoven = new Dog("Beethoven", "George");
+const beethoven = new Dog("Beethoven", "George", "Hello!");
 beethoven.speak();
 beethoven.move(10);
 beethoven.belongsTo();
 
-const hachi = new Dog("Hachi", "Parker");
+const hachi = new Dog("Hachi", "Parker", "Hello!");
 hachi.speak();
 hachi.move(10);
 hachi.belongsTo();
 
-const scoobyDoo = new Dog("Scooby-Doo", "Shaggy");
+const scoobyDoo = new Dog("Scooby-Doo", "Shaggy", "Hello!");
 scoobyDoo.speak();
 scoobyDoo.move(10);
 scoobyDoo.belongsTo();
