e7a84758bc8913f7aaf60ca8a1120b4bc18c6527
Bakhytzhan Abeldinov <bakhytzhan79@mail.ru>
2022-12-08 22:55:22 +0600

    add change

--- file10.js
+++ file10.js
@@ -15,3 +15,24 @@ function multiply2Max(a) {
 console.log(multiply2Max(1)(2)(3)); // 2
 console.log(multiply2Max(4)(4)(1)); // 4
 console.log(multiply2Max(3)(0)(5)); // 0
+
+
+let arr = [];
+function multiply2Max(a) {
+  return function(b) {
+      return function(c) {
+          arr = [a, b, c];
+          arr.sort((a, b) => a - b);
+          for (let elem of arr) {
+            if (elem % 2 === 0) {
+              return elem;
+            }
+          }
+          return arr;
+      };
+  };
+}
+
+console.log(multiply2Max(1)(2)(3)); // 2
+console.log(multiply2Max(4)(4)(1)); // 4
+console.log(multiply2Max(3)(0)(5)); // 0
